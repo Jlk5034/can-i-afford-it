adbf9e4856eae70b213e3fef391431a11712b6de
Jlk5034 <jlk5034@gmail.com>
2025-08-07 14:26:48 -0400

    Fix build - remove PWA temporarily

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['via.placeholder.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
